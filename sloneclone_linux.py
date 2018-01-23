6575d4d3cbf08b6e1159c646c465852e074344cf
Samuel Furtaw <samfurtaw@gmail.com>
2018-01-23 17:04:17 -0500

    Add more contraband software and reformat

--- sloneclone_linux.py
+++ sloneclone_linux.py
@@ -12,69 +12,84 @@ import shutil
 
 #-------------- COLORS
 # Nice colors definitely not stolen from Blender
-colors = {'header': '\033[95m', 'okblue': '\033[94m',\
- 'okgreen': '\033[92m', 'warning': '\033[91m', 'fail': '\033[91m', 'endc': '\033[0m',\
- 'bold': '\033[1m', 'underline': '\033[4m'}
+colors = {'header': '\033[95m', 'okblue': '\033[94m',
+          'okgreen': '\033[92m', 'warning': '\033[91m', 'fail': '\033[91m', 'endc': '\033[0m',
+          'bold': '\033[1m', 'underline': '\033[4m'}
 
 #-------------- VARS
 # Set temp variables for later
 mediaFiles = apps = distribution = delUser = addUser = group\
- = demote = exceptionPort = sudoers = confirm = "temp"
+    = demote = exceptionPort = sudoers = confirm = "temp"
 
 #-------------- FUNCTIONS
+
+
 def call(command):
     """Call a command as a subprocess"""
     return subprocess.call(str(command), shell=True)
+
 
 def runProcess(command):
     """Call a subprocess, silence output/check output as variable"""
     return subprocess.check_output(str(command), shell=True, universal_newlines=True)
 
+
 def fail(text):
     """Set failure color and print text"""
     print(colors['fail'] + str(text) + colors['endc'])
+
 
 def blue(text):
     """Set blue color and print text"""
     print(colors['okblue'] + str(text) + colors['endc'])
 
+
 def green(text):
     """Set OK (green) color and print text"""
     print(colors['okgreen'] + str(text) + colors['endc'])
+
 
 def warning(text):
     """Set warning color and print text"""
     print(colors['warning'] + str(text) + colors['endc'])
 
+
 def getBoldInput(text):
     """"Ask for input, set bold color"""
     return input(colors['bold'] + str(text) + colors['endc'])
+
 
 def bold(text):
     """Print text in bold"""
     print(colors['bold'] + str(text) + colors['endc'])
 
+
 def underline(text):
     """Print text underlined"""
     print(colors['underline'] + str(text) + colors['endc'])
 
+
 def uninstall(uninstalls):
     """Take a list and attempt to uninstall all packages in list"""
     try:
-        bold("Removing " + "/".join(uninstalls) + " using " + aptCommand + " ...")
+        bold("Removing " + "/".join(uninstalls) +
+             " using " + aptCommand + " ...")
         for uninstallApp in uninstalls:
             call(aptCommand + " purge -y " + str(uninstallApp))
     except:
         fail("Removing " + "/".join(list) + " failed.")
 
+
 def install(installs):
     """Take a list and attempt to install all packages in list"""
     try:
-        bold("Installing " + "/".join(installs) + " using " + str(aptCommand) + "...")
+        bold("Installing " + "/".join(installs) +
+             " using " + str(aptCommand) + "...")
         for installapp in installs:
             call(str(aptCommand) + " install -y " + str(installapp))
     except:
         fail("Installing " + "/".join(installs) + " failed.")
+
 
 def restartService(service):
     """Restart service with distribution-specific command"""
@@ -85,6 +100,7 @@ def restartService(service):
         bold("Restarting " + str(service) + " via upstart...")
         call("service " + str(service) + " restart")
 
+
 def disableService(service):
     """Disable service with distribution-specific command"""
     if os.path.exists("/bin/systemctl"):
@@ -93,6 +109,7 @@ def disableService(service):
     elif os.path.exists("/usr/sbin/service"):
         bold("Disabling " + str(service) + " via upstart...")
         runProcess("echo manual > /etc/init/" + str(service) + ".override")
+
 
 #-------------- CHECKS
 # Check for 'debug' command-line argument for testing
@@ -105,8 +122,8 @@ elif platform.system() != 'Linux':
     sys.exit()
 # Make sure Python version 3 is in use, error out if otherwise
 elif list(sys.version_info)[0] < 3:
-    fail("Python version " + platform.python_version() + \
-    " is unsupported. Python version must be 3+. Exiting...")
+    fail("Python version " + platform.python_version() +
+         " is unsupported. Python version must be 3+. Exiting...")
     sys.exit()
 # Check the user is root
 elif getpass.getuser() != 'root':
@@ -138,9 +155,9 @@ if len(sys.argv) > 1 and "brute" in sys.argv:
 #print("[2] Debian 7 Wheezy")
 #print("[3] Ubuntu 16.04 Xenial Xerus")
 #print("[4] Debian 8 Jessie")
-#print("\n")
+# print("\n")
 #distribution = boldinput("Input distribution: ")
-distributions = {1:"trusty", 2:"wheezy", 3:"xenial", 4:"jessie"}
+distributions = {1: "trusty", 2: "wheezy", 3: "xenial", 4: "jessie"}
 if os.path.exists("/usr/bin/lsb_release"):
     green("LSB_Release was found")
     lsb = str(runProcess("lsb_release -a"))
@@ -181,38 +198,39 @@ else:
 if "skipapt" in sys.argv:
     warning("Skipping apt sections.")
 else:
-#-------------- APT SOURCES
-# Consistently break APT, TODO
-#	if distribution == 1:
-#		sources = ["deb http://security.ubuntu.com/ubuntu/ trusty main restricted\n","deb http://security.ubuntu.com/ubuntu/ trusty-security universe\n","deb http://security.ubuntu.com/ubuntu/ trusty-security multiverse"]
-#	elif distribution == 2:
-#		sources = ["deb http://ftp.us.debian.org/debian wheezy main\n"]
-#	elif distribution == 3:
-#		sources = ["deb http://security.ubuntu.com/ubuntu/ xenial main restricted\ndeb http://security.ubuntu.com/ubuntu/ xenial-security universe restricted main\ndeb http://archive.ubuntu.com/ubuntu xenial main universe restricted multiverse\ndeb http://archive.ubuntu.com/ubuntu xenial-updates main restricted multiverse universe\ndeb http://security.ubuntu.com/ubuntu/ xenial-security multiverse"]
-#	elif distribution == 4:
-#		sources = ["deb http://ftp.us.debian.org/debian jessie main\n","deb http://security.debian.org/ jessie/updates main\n"]
-#
-# Move sources.list to backup
-#	try:
-#		shutil.move("/etc/apt/sources.list","/etc/apt/sources.list.old")
-#		green("Sources.list moved to /etc/apt/sources.list.old")
-#	except:
-#		fail("Sources.list move failed.")
-#
-# Create a new sources.list and write the sources
-#	try:
-#		sourcelist = open("/etc/apt/sources.list","w+")
-#		sourcelist.writelines(sources)
-#		green("New sources have been written")
-# Close sources.list
-#		sourcelist.close()
-#	except:
-#		fail("Writing sources.list failed.")
+    #-------------- APT SOURCES
+    # Consistently breaks APT, TODO
+    #	if distribution == 1:
+    #		sources = ["deb http://security.ubuntu.com/ubuntu/ trusty main restricted\n","deb http://security.ubuntu.com/ubuntu/ trusty-security universe\n","deb http://security.ubuntu.com/ubuntu/ trusty-security multiverse"]
+    #	elif distribution == 2:
+    #		sources = ["deb http://ftp.us.debian.org/debian wheezy main\n"]
+    #	elif distribution == 3:
+    #		sources = ["deb http://security.ubuntu.com/ubuntu/ xenial main restricted\ndeb http://security.ubuntu.com/ubuntu/ xenial-security universe restricted main\ndeb http://archive.ubuntu.com/ubuntu xenial main universe restricted multiverse\ndeb http://archive.ubuntu.com/ubuntu xenial-updates main restricted multiverse universe\ndeb http://security.ubuntu.com/ubuntu/ xenial-security multiverse"]
+    #	elif distribution == 4:
+    #		sources = ["deb http://ftp.us.debian.org/debian jessie main\n","deb http://security.debian.org/ jessie/updates main\n"]
+    #
+    # Move sources.list to backup
+    #	try:
+    #		shutil.move("/etc/apt/sources.list","/etc/apt/sources.list.old")
+    #		green("Sources.list moved to /etc/apt/sources.list.old")
+    #	except:
+    #		fail("Sources.list move failed.")
+    #
+    # Create a new sources.list and write the sources
+    #	try:
+    #		sourcelist = open("/etc/apt/sources.list","w+")
+    #		sourcelist.writelines(sources)
+    #		green("New sources have been written")
+    # Close sources.list
+    #		sourcelist.close()
+    #	except:
+    #		fail("Writing sources.list failed.")
 
-#-------------- UNAUTHORIZED SOFTWARE
-# Smack down the banhammer
-    taboo = ["vuze", "transmission-gtk", "transmission-common", "john", "john-data",\
-    "hydra-gtk", "hydra", "frost", "ophcrack", "nikto"]
+    #-------------- UNAUTHORIZED SOFTWARE
+    # Smack down the banhammer
+    taboo = ["vuze", "transmission-gtk", "transmission-common", "john", "john-data",
+             "hydra-gtk", "hydra", "frost", "ophcrack", "nikto", "medusa", "minetest", "minetest-data"
+             "minetest-server"]
     uninstall(taboo)
 
 #-------------- PACKAGE UPDATES
@@ -223,8 +241,8 @@ else:
         bold("Updating packages...")
         runProcess("apt-get upgrade -y ")
 # Editable list to add new applications to
-        autoInstall = ["clamav", "nano", "vim", "ufw",\
-        "unattended-upgrades", "nmap", "openssh-server"]
+        autoInstall = ["clamav", "nano", "vim", "ufw",
+                       "unattended-upgrades", "nmap", "openssh-server"]
         install(autoInstall)
 # Kind message to the user if it errors out
     except:
@@ -233,7 +251,8 @@ else:
 #-------------- PACKAGE INSTALLATION
 # Fun while loop to install apps through Aptitude
     while apps != "":
-        apps = str(getBoldInput("Packages to install (separated by a space): ")).split()
+        apps = str(getBoldInput(
+            "Packages to install (separated by a space): ")).split()
         if str(apps) == "[]":
             break
         try:
@@ -269,13 +288,15 @@ while delUser != "":
 #-------------- ADD USERS
 # Accept user and group in one input
 while addUser != "":
-    addUser = getBoldInput("User name and group to add, separated by a space: ")
+    addUser = getBoldInput(
+        "User name and group to add, separated by a space: ")
     if addUser == "":
         break
     userGroup = addUser.split()
     runProcess("adduser " + str(userGroup[0]))
     if len(userGroup) > 1:
-        runProcess("usermod -a -G " + str(userGroup[1]) + " " + str(userGroup[0]))
+        runProcess("usermod -a -G " +
+                   str(userGroup[1]) + " " + str(userGroup[0]))
 
 #-------------- DEMOTE USERS
 # Ask for users to demote
@@ -321,7 +342,8 @@ try:
     runProcess("grep -v disable_ipv6 /etc/sysctl.conf > /tmp/sysctl.temp")
     shutil.move("/tmp/sysctl.temp", "/etc/sysctl.conf")
     sysctl = open("/etc/sysctl.conf", "a+")
-    sysctl.write("net.ipv6.conf.all.disable_ipv6=1\nnet.ipv6.conf.default.disable_ipv6=1")
+    sysctl.write(
+        "net.ipv6.conf.all.disable_ipv6=1\nnet.ipv6.conf.default.disable_ipv6=1")
     sysctl.close()
 except:
     fail("SysCTL configuration failed.")
@@ -348,7 +370,8 @@ for dirPath, dirNames, fileNames in os.walk("/etc/sudoers.d/"):
     for fileName in fileNames:
         blue(fileName)
 while sudoers != "":
-    sudoers = getBoldInput("Sudoers.d findings above, enter filename to edit: ")
+    sudoers = getBoldInput(
+        "Sudoers.d findings above, enter filename to edit: ")
     if sudoers == "":
         break
     call("nano /etc/sudoers.d/" + sudoers)
@@ -357,8 +380,10 @@ while sudoers != "":
 # Remove comments and change parameters with sed in login.defs
 try:
     runProcess("sed -i -e 's/.*#.*/#/' /etc/login.defs")
-    runProcess("sed -i -e 's/.*PASS_MAX_DAYS.*/PASS_MAX_DAYS   30/' /etc/login.defs")
-    runProcess("sed -i -e 's/.*PASS_MIN_DAYS.*/PASS_MIN_DAYS   7/' /etc/login.defs")
+    runProcess(
+        "sed -i -e 's/.*PASS_MAX_DAYS.*/PASS_MAX_DAYS   30/' /etc/login.defs")
+    runProcess(
+        "sed -i -e 's/.*PASS_MIN_DAYS.*/PASS_MIN_DAYS   7/' /etc/login.defs")
     runProcess("faillog -m 3")
 # Line below breaks logins
 #os.system("sed -i '1 i\auth required pam_tally.so per_user magic_root onerr=fail' /etc/pam.d/common-auth")
@@ -407,7 +432,7 @@ getBoldInput("Paused, strike return to continue")
 
 #-------------- SERVERS
 packageList = str(runProcess("dpkg --list"))
-### OPENSSH
+# OPENSSH
 if "openssh-server" in packageList:
     bold("OpenSSH server is installed.")
 # Call sed to remove comments, then edit in the new parameters
@@ -416,24 +441,30 @@ if "openssh-server" in packageList:
         green("Removed comments.")
         runProcess("sed -i -e 's/.*Protocol.*/Protocol 2/' /etc/ssh/sshd_config")
         green("Forced SSHv2.")
-        runProcess("sed -i -e 's/.*PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config")
+        runProcess(
+            "sed -i -e 's/.*PermitRootLogin.*/PermitRootLogin no/' /etc/ssh/sshd_config")
         green("Disabled root login.")
-        runProcess("sed -i -e 's/.*LoginGraceTime*.*/LoginGraceTime 30/g' /etc/ssh/sshd_config")
+        runProcess(
+            "sed -i -e 's/.*LoginGraceTime*.*/LoginGraceTime 30/g' /etc/ssh/sshd_config")
         green("Reduced login grace time.")
-        runProcess("sed -i -e 's/.*StrictModes.*/StrictModes yes/' /etc/ssh/sshd_config")
+        runProcess(
+            "sed -i -e 's/.*StrictModes.*/StrictModes yes/' /etc/ssh/sshd_config")
         green("Required strict modes.")
         runProcess("sed -i -e 's/.*PermitEmptyPasswords.*/PermitEmptyPasswords no/'\
         /etc/ssh/sshd_config")
         green("Disabled empty passwords.")
-        runProcess("sed -i -e 's/.*X11Forwarding.*/X11Forwarding no/' /etc/ssh/sshd_config")
+        runProcess(
+            "sed -i -e 's/.*X11Forwarding.*/X11Forwarding no/' /etc/ssh/sshd_config")
         green("Disabled X forwarding.")
-        runProcess("sed -i -e 's/.*PrintMotd no.*/PrintMotd yes/g' /etc/ssh/sshd_config")
+        runProcess(
+            "sed -i -e 's/.*PrintMotd no.*/PrintMotd yes/g' /etc/ssh/sshd_config")
         green("Enabled MOTD.")
         green("OpenSSH configuration done.")
     except:
         fail("SSHD config failed.")
-else: warning("OpenSSH server is not installed.")
-### DROPBEAR
+else:
+    warning("OpenSSH server is not installed.")
+# DROPBEAR
 if "dropbear" in packageList:
     warning("Dropbear is installed.")
     dropbear = getBoldInput("Remove Dropbear? (y/N): ")
@@ -441,14 +472,16 @@ if "dropbear" in packageList:
         uninstall(["dropbear"])
 else:
     bold("Dropbear is not installed.")
-### MYSQL
+# MYSQL
 if "mysql-server" in packageList:
     bold("MySQL is installed.")
     try:
-# Allow only local access to MySQL in my.cnf
-        runProcess("sed -i -e 's/.*bind-address=.*/bind-address=127.0.0.1/' /etc/mysql/my.cnf")
+        # Allow only local access to MySQL in my.cnf
+        runProcess(
+            "sed -i -e 's/.*bind-address=.*/bind-address=127.0.0.1/' /etc/mysql/my.cnf")
         green("Bind address set.")
-        runProcess("sed -i -e 's/.*local-infile=.*/local-infile=0/' /etc/mysql/my.cnf")
+        runProcess(
+            "sed -i -e 's/.*local-infile=.*/local-infile=0/' /etc/mysql/my.cnf")
         green("Disabled use of local files.")
         call("mysql_secure_installation")
         restartService("mysql")
@@ -457,25 +490,30 @@ if "mysql-server" in packageList:
         fail("MySQL configuration failed.")
 else:
     bold("MySQL is not installed.")
-### APACHE
+# APACHE
 if "apache2" in packageList:
     bold("Apache2 is installed.")
     try:
-        runProcess("sed -i -e 's/.*#.*/#/' /etc/apache2/conf-enabled/security.conf")
+        runProcess(
+            "sed -i -e 's/.*#.*/#/' /etc/apache2/conf-enabled/security.conf")
         green("Removed comments.")
-        runProcess("sed -i -e 's/.*ServerTokens.*/#/' /etc/apache2/conf-enabled/security.conf")
+        runProcess(
+            "sed -i -e 's/.*ServerTokens.*/#/' /etc/apache2/conf-enabled/security.conf")
         green("Deleted ServerTokens directive.")
-        runProcess("sed -i -e 's/.*ServerSignature.*/#/' /etc/apache2/conf-enabled/security.conf")
+        runProcess(
+            "sed -i -e 's/.*ServerSignature.*/#/' /etc/apache2/conf-enabled/security.conf")
         green("Deleted ServerSignature directive.")
-        runProcess("echo 'ServerTokens Prod' >> /etc/apache2/conf-enabled/security.conf")
+        runProcess(
+            "echo 'ServerTokens Prod' >> /etc/apache2/conf-enabled/security.conf")
         green("Set ServerTokens to production mode.")
-        runProcess("echo 'ServerSignature Off' >> /etc/apache2/conf-enabled/security.conf")
+        runProcess(
+            "echo 'ServerSignature Off' >> /etc/apache2/conf-enabled/security.conf")
         green("Disabled ServerSignature.")
         restartService("apache2")
         green("Apache2 configuration done.")
     except:
         fail("Apache2 configuration failed.")
-### SAMBA
+# SAMBA
 if os.path.exists("/usr/sbin/smbd"):
     bold("Samba is installed.")
     try:
@@ -490,20 +528,20 @@ if os.path.exists("/usr/sbin/smbd"):
     except:
         fail("Samba configuration failed.")
     green("Samba configuration done.")
-### PHP
+# PHP
 if os.path.exists("/usr/bin/php"):
     bold("PHP is installed.")
-    ######## PHP CONFIGURATION HERE
+    # PHP CONFIGURATION HERE
     green("PHP configuration done.")
 
-### FTP
-    ######## FTP CONFIGURATION HERE
+# FTP
+    # FTP CONFIGURATION HERE
 
-### NFS
-    ######## NFS CONFIGURATION HERE
+# NFS
+    # NFS CONFIGURATION HERE
 
-### VNC
-    ######## VNC CONFIGURATION HERE
+# VNC
+    # VNC CONFIGURATION HERE
 
 #-------------- MEDIA FILES
 if "skipmedia" in sys.argv:
